4c8fb2856e15a9eb15f1ff19ac38ff356f95aa92
Michael Skrzypczak <michael.skrzypczak89@gmail.com>
2022-10-18 21:34:17 -0600

    Update 10/18/22 10:34pm

--- src/controller/AddAppointment.java
+++ src/controller/AddAppointment.java
@@ -2,18 +2,57 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import model.Contacts;
+import model.Customers;
+import model.User;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalTime;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
-public class AddAppointment {
+public class AddAppointment implements Initializable {
+
+    public TextField ApptIdText;
+    public TextField TitleText;
+    public TextField DescriptionText;
+    public TextField LocationText;
+    public DatePicker StartDateDatePicker;
+    public ComboBox<LocalTime> StartTimeCombo;
+    public ComboBox<Contacts> ContactCombo;
+    public TextField TypeText;
+    public ComboBox<Customers> CustIdCombo;
+    public ComboBox<User> UserIdCombo;
+    public DatePicker EndDateDatePicker;
+    public ComboBox<LocalTime> EndTimeCombo;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
     public void onSaveAppt(ActionEvent actionEvent) {
+        try {
+         /*   double price = Double.parseDouble(addPartPriceText.getText());
+            int stock = Integer.parseInt(addPartInvText.getText());
+            int min = Integer.parseInt(addPartMinText.getText());
+            int max = Integer.parseInt(addPartMaxText.getText());
+          */
+
+        } catch(Exception e) {
+                e.printStackTrace();
+        }
     }
 
     public void onCancelAppt(ActionEvent actionEvent) throws IOException {
@@ -31,4 +70,6 @@ public class AddAppointment {
             stage.show();
         }
     }
+
+
 }
